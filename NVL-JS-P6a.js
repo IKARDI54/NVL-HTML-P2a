f76f7022ab4d3341b156d0ff8be22511f2b66eab
IKARDI54 <46534703+IKARDI54@users.noreply.github.com>
2019-01-28 17:48:07 +0100

    startjs

---
+++ NVL-JS-P6a.js
@@ -0,0 +1,13 @@
+var nameyapellidos='Israel Sanchez Torrero';
+var edad= 35;
+var díasdelasemana= ['lunes','martes', 'miercoles',
+'jueves', 'viernes', 'sabado', 'domingo'];
+var valordelagravedad= 9.8;
+var primerosimpares= [1,3,5,7];
+/*
+Tu nombre y apellidos
+Tu edad
+Los días de la semana
+El valor de la gravedad
+Los 3 primeros números impares
+ */
